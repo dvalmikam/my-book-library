83d7efa02012f5f08009b9242143b42b22da1892
Deepthi Valmikam <deepthivalmikam@yahoo.com>
2018-05-07 10:50:36 -0400

    commit3

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
+
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('server started '+ port);
