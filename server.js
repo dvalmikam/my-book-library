bdbdac2d93226ea977b69c11487be6007c8c253f
Deepthi Valmikam <deepthivalmikam@yahoo.com>
2018-05-07 11:02:34 -0400

    commit4

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname));
-
+//app.use(serveStatic(__dirname));
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
 // Catch all routes and redirect to the index file
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/dist/index.html')
