e7fe32a3c64f02aaf1e7b47a1c3c6ed0368f77ed
Deepthi Valmikam <deepthivalmikam@yahoo.com>
2018-05-07 11:34:32 -0400

    commit5

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname));
+app.use(serveStatic(__dirname+"/dist"));
+//app.use(serveStatic(__dirname));
 //app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
 // Catch all routes and redirect to the index file
-app.get('*', function (req, res) {
-    res.sendFile('/index.html')
-})
+// app.get('*', function (req, res) {
+//     res.sendFile('/index.html')
+// })
 
 var port = process.env.PORT || 3000;
 app.listen(port);
